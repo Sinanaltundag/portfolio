86ec6f1ceba99d3f201d06ddf2305cfd673af6c8
peykani <peykani@gmail.com>
2022-04-17 11:04:12 +0300

    playground

--- src/components/ClassNotes/ClassNotes.jsx
+++ src/components/ClassNotes/ClassNotes.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import {ReactComponent as SvgRe} from "../../assets/svg/react-2.svg"
 import SvgRes from "../../assets/svg/react-2.svg"
+import { cssSvg, GoSvg, HtmlSvg, ReactIcon, ReactSvg } from '../../assets/svg/SvgIcons'
 
 const ClassNotes = () => {
+
+  const a=1
+  if (1==a) {
+    console.log(a)
+  }
 
   const [coor, setCoor] = useState([])
 
@@ -15,8 +21,13 @@ setCoor([e.pageX/10, e.pageY/10])
     <div onMouseMove={handleMove}>
 
 <img src={SvgRes} alt="" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
+<img src={cssSvg} alt="" />
+<HtmlSvg width="100" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
 
-        
+<ReactSvg width="500" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
+<GoSvg width="150"/>
+
+
 <br />
 <br />
 <br />
