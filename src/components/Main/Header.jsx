e66a50d4f917e76d4c3886670956c04532298a08
peykani <peykani@gmail.com>
2022-04-18 18:25:46 +0300

    codes moved to component page

--- src/components/Main/Header.jsx
+++ src/components/Main/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
     
-    <Box  sx={{ width:"100vw", height:"100vh", textAlign: 'start', backgroundImage:`url(${HeaderBg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundAttachment:"fixed", display: "flex", alignItems: "center", color: "darkred", textShadow:"0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15)", boxShadow: "0px 0 55px 50px rgba(0, 0, 0, 0.8)", borderRadius:  "50%", margin: "50px"}}  >
+    <Box  sx={{ width:"90vw", height:"90vh", textAlign: 'start', backgroundImage:`url(${HeaderBg})`, backgroundSize: "cover", backgroundPosition: "center", backgroundAttachment:"fixed", display: "flex", alignItems: "center", color: "primary.dark", textShadow:"0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15)", boxShadow: "0px 0 55px 50px rgba(0, 0, 0, 0.8)", borderRadius:  "50% 20% / 10% 40%;"}}  >
     <Typography variant="h3" gutterBottom component="div" sx={{padding: '100px'}}>
        <span>Hello Everybody!</span> 
        <Typewriter
