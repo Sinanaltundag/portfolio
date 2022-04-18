2905f9ac7da48018d985f1e8850af5dc5136d283
peykani <peykani@gmail.com>
2022-04-18 18:24:55 +0300

    playground

--- src/components/ClassNotes/ClassNotes.jsx
+++ src/components/ClassNotes/ClassNotes.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import {ReactComponent as SvgRe} from "../../assets/svg/react-2.svg"
 import SvgRes from "../../assets/svg/react-2.svg"
-import { cssSvg, GoSvg, HtmlSvg, ReactIcon, ReactSvg } from '../../assets/svg/SvgIcons'
+import { CssSvg, GoSvg, HtmlSvg, ReactIcon, ReactSvg } from '../../assets/svg/SvgIcons'
+import sassSvg from "../../assets/svg/sass-1.svg"
+
+
 
 const ClassNotes = () => {
 
@@ -21,12 +24,13 @@ setCoor([e.pageX/10, e.pageY/10])
     <div onMouseMove={handleMove}>
 
 <img src={SvgRes} alt="" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
-<img src={cssSvg} alt="" />
+<img src={CssSvg} alt="" />
 <HtmlSvg width="100" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
 
 <ReactSvg width="500" style={{filter: `drop-shadow( -${coor[0]}px -${coor[1]}px 2px rgba(0, 0, 0, .7))`}}/>
 <GoSvg width="150"/>
 
+<img src={sassSvg} alt="" style={{width: '100px'}} />
 
 <br />
 <br />
