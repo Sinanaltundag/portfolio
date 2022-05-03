087e9e39be21edb5c28a6536af8e99e9416d29ea
peykani <peykani@gmail.com>
2022-05-03 17:13:40 +0300

    skill page dynamic reusable comp. responsive adj

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,8 +1,15 @@
 import { push, ref, remove, set, update } from "firebase/database";
-import  { useContext, useState,  createContext } from "react";
+import { useContext, useState, createContext } from "react";
 import { toast } from "react-toastify";
 import { firebaseDB } from "../helpers/firebaseConnect";
-import movieSm from "../assets/react-movie-sm.jpg"
+import {
+  HtmlSvg,
+  JsSvg,
+  ReactSvg,
+  BsSvg,
+  SassSvg,
+  MuiSvg,
+} from "../assets/svg/SvgIcons";
 
 const BlogContext = createContext();
 
@@ -12,10 +19,9 @@ export function useBlog() {
 
 export function BlogProvider({ children }) {
   const [currentBlogs, setCurrentBlogs] = useState();
-const [activeTopic, setActiveTopic] = useState("react")  
+  const [activeTopic, setActiveTopic] = useState("react");
 
-  function addBlog(newBlog,dbName) {
-    
+  function addBlog(newBlog, dbName) {
     const blogRef = ref(firebaseDB, dbName);
     const newBlogRef = push(blogRef);
     set(newBlogRef, newBlog);
@@ -28,7 +34,7 @@ const [activeTopic, setActiveTopic] = useState("react")
 
   function deleteOneBlog(id, dbName) {
     remove(ref(firebaseDB, dbName + "/" + id));
-  toast("Record Deleted");
+    toast("Record Deleted");
   }
 
   function editBlog(blog, dbName) {
@@ -38,7 +44,7 @@ const [activeTopic, setActiveTopic] = useState("react")
     return update(ref(firebaseDB), updates);
   }
 
-/*   useEffect(() => {
+  /*   useEffect(() => {
     setIsLoading(true);
     
     const blogRef = ref(firebaseDB, "blogdata");
@@ -54,33 +60,102 @@ const [activeTopic, setActiveTopic] = useState("react")
   }, []); */
   const reactProjects = [
     {
-      img: {movieSm},
-      url: "",
-      title: 'Breakfast',
-      author: '@Sinan',
-      details:"",
+      img: "./images/react-movie-sm.jpg",
+      title: "Movie App",
+      author: "@Sinan",
+      details: [
+        "Project aims to create a Movie App.",
+        "Fetch api from TMDB",
+        "List data on landing page",
+        "Show filtered data according to search terms",
+        "Login function with firebase (email / with google)",
+        "Guests can't see movie details",
+        "Pagination",
+        "BootStrap 5",
+      ],
+      url: "https://firebase-movie.herokuapp.com/",
+      aim: "Private Routing, Bootstrap, Firebase auth, Context, pagination",
     },
     {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-      author: '@rollelflex_graphy726',
+      img: "./images/react-myblog-sm.jpg",
+      title: "Blog App",
+      author: "@Sinan",
+      details: [
+        "Firebase authentication, Realtime database",
+        "State management with Redux",
+        "React public and private router",
+        "Blog Add, delete, update functions",
+        "Styled with Material UI",
+        "Guests can not see details",
+        "Add favorites, comments functions",
+        "Logged in users can see their favorites and contributions on Profile page",
+      ],
+      url: "https://myblogdb-app.netlify.app/",
+      aim: "Firebase auth, Realtime database, Redux, Router, Material ui, Responsive",
     },
     {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-      author: '@helloimnik',
+      img: "./images/react-recipe-sm.jpg",
+      title: "Recipe App",
+      author: "@Sinan",
+      details: [
+        "Using api and manipulation data",
+        "Routing",
+        "Simple user auth",
+        "Search on api",
+        "Styled-components",
+      ],
+      url: "https://sinan-recipe-app.netlify.app/",
+      aim: "Routing, Styled-Components, Fetch api",
     },
-     {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
-      author: '@arwinneil',
-      rows: 2,
-      cols: 2,
-      featured: true,
+    {
+      img: "./images/react-webtemplate-sm.jpg",
+      title: "Website Template",
+      author: "@Sinan",
+      details: [
+        "There are three pages controlled router",
+        "Responsive design with css",
+        "Simple Reusable Components",
+        "Toastify",
+      ],
+      url: "https://router-website-template.netlify.app/",
+      aim: "Responsive, Router, Template",
     },
-  
   ];
-
+//! dynamic svg control
+  const skillListFrontend = [
+    {
+      title: "REACT",
+      subjects: ["Hooks", "Styled Components", "Material UI", "Custom Hooks"],
+      svg: <ReactSvg />,
+    },
+    {
+      title: "JavaScript",
+      subjects: ["DOM", "Events", "Loop Methods", "Promises", "Clean Code"],
+      svg: <JsSvg />,
+    },
+    {
+      title: "HTML5",
+      subjects: ["Semantic Markup", "Forms", "Tables", "Web Storages"],
+      svg: <HtmlSvg />,
+    },
+  ];
+  const skillListStyling = [
+    {
+      title: "Sass",
+      subjects: ["Variables", "@mixins", "@extends", "Modules"],
+      svg: <SassSvg />,
+    },
+    {
+      title: "BootStrap",
+      subjects: ["Components", "Forms", "Customize", "Sass", "Responsive"],
+      svg: <BsSvg />,
+    },
+    {
+      title: "Material UI",
+      subjects: ["Components", "Custom Components", "Themes", "Responsive"],
+      svg: <MuiSvg />,
+    },
+  ];
 
   const value = {
     addBlog,
@@ -89,9 +164,11 @@ const [activeTopic, setActiveTopic] = useState("react")
     deleteOneBlog,
     editBlog,
     setCurrentBlogs,
-    activeTopic, 
+    activeTopic,
     setActiveTopic,
     reactProjects,
+    skillListStyling,
+    skillListFrontend,
   };
 
   return <BlogContext.Provider value={value}>{children}</BlogContext.Provider>;
--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -1,115 +1,135 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Box, CardActionArea, Container, Grid, Paper} from '@mui/material';
-import { styled } from '@mui/material/styles';
-import reactSvg from "../../assets/svg/react-2.svg"
-import sassSvg from "../../assets/svg/sass-1.svg"
-import { useState } from 'react';
-import SkillList from './mainComponents/SkillList';
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, CardActionArea, Container, Grid, Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import reactSvg from "../../assets/svg/react-2.svg";
+import sassSvg from "../../assets/svg/sass-1.svg";
+import { useState } from "react";
+import SkillList from "./mainComponents/SkillList";
+import { useTheme } from "../../Context/ThemeContext";
+import { useBlog } from "../../Context/DataContext";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
-const Skills = ({width1200}) => {
-  const [coor, setCoor] = useState([])
-  
-  const handleMove =(e)=>{
-  setCoor([-e.screenX/100, -e.screenY/100])
-  }
+const Skills = ({ width1200 }) => {
+  const [coor, setCoor] = useState([]);
+const {width600}=useTheme();
+const {skillListFrontend,skillListStyling}=useBlog();
+  const handleMove = (e) => {
+    setCoor([-e.screenX / 100, -e.screenY / 100]);
+  };
   // const [scoor, setScoor] = useState([])
 
-
   return (
-    <Grid container spacing={2} width="80vw" height="auto" sx={{overflowY:"auto",gridAutoFlow: "column"}}  onMouseMove={handleMove} >
-    <Grid item md={12}>
-    <Typography variant="h2" color="text.secondary">
-      SKILLS
-      </Typography>
-    </Grid>
-        {/* row height  equalized */}
+    <Grid
+      container
+      spacing={2}
+      width={`${width600?"100%":"80vw"}`}
+      height="auto"
+      sx={{ overflowY: "auto", gridAutoFlow: "column" }}
+      onMouseMove={handleMove}
+    >
+      <Grid item md={12}>
+        <Typography variant="h2" color="text.secondary">
+          SKILLS
+        </Typography>
+      </Grid>
 
-    <Grid item lg={7} sm={12} sx={{display:"grid", alignItems:"stretch"}}>
-      {/* <Item sx={ {height: '85%'}} > */}
-      <Item sx={ {height: 'auto'}} >
-      <Container >
-      {/* <img src={svgs.ReactSvg} width="10px" alt="" /> */}
-      <SkillList/>
-      
-       </Container>
-      </Item>
-    </Grid>
-    
-    <Grid item lg={5} sm={12} >
-    <Card sx={{ minWidth: 345 }}>
-      <CardActionArea sx={{display: `${width1200?"flex":"inline"}` }}>
-      <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', width: '100%',  filter:`drop-shadow(  ${coor[0]}px ${coor[1]}px  5px rgba(0,0,0,0.5))`}}>
-      
-        <CardMedia
-        sx={{ textShadow:"2px 2px 2px rgba(0,0,0)"}}
-          component="svg"
-          image={reactSvg}
-          alt="react"
-          width="140px"
-        />
-        <Typography variant="caption" color="text.secondary" fontSize="3rem">
-      REACT
-      </Typography>
-        
-        </Box>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Frontend
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Most popular component based Javascript library for building efective and fast user interfaces.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Grid>
-    <Grid item lg={5} sm={12} >
-    <Card sx={{ minWidth: 345 }}>
-      <CardActionArea sx={{display: `${width1200?"flex":"inline"}` }}>
-      <Box sx={{display: 'flex', padding: "15px", alignItems: 'center', justifyContent: 'space-evenly', width: '100%',  filter:`drop-shadow(  ${coor[0]}px ${coor[1]}px  5px rgba(0,0,0,0.5))`}}>
-      
-        <CardMedia
-        sx={{ textShadow:"2px 2px 2px rgba(0,0,0)"}}
-          component="svg"
-          image={sassSvg}
-          alt="sass"
-          width="200px"
-        />
-        <Typography variant="h4" color="text.secondary" fontSize="3rem">
-      SASS
-      </Typography>
-        
-        </Box>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Styling
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Most popular styling language used with most of libraries.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Grid>
-    {/* row height  equalized */}
-    <Grid item lg={7} sm={12} sx={{display:"grid", alignItems:"stretch"}}>
-      <Item >
-      <SkillList/>
-      </Item>
-    </Grid>
-  </Grid>
-  )
-}
+      <Grid item lg={7} sm={12} sx={{ display: "grid", alignItems: "stretch" }}>
+        <Item sx={{ height: "auto" }}>
+          <SkillList skillList={skillListFrontend}/>
+        </Item>
+      </Grid>
 
-export default Skills
+      <Grid item lg={5} sm={12}>
+        <Card sx={{ minWidth: 345 }}>
+          <CardActionArea sx={{ display: `${width1200 ? "flex" : "inline"}` }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                width: "100%",
+                filter: `drop-shadow(  ${coor[0]}px ${coor[1]}px  5px rgba(0,0,0,0.5))`,
+              }}
+            >
+              <CardMedia
+                sx={{ textShadow: "2px 2px 2px rgba(0,0,0)" }}
+                component="svg"
+                image={reactSvg}
+                alt="react"
+                width="140px"
+              />
+              {!width600&&<Typography
+                variant="caption"
+                color="text.secondary"
+                fontSize="3rem"
+              >
+                REACT
+              </Typography>}
+            </Box>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Frontend
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Most popular component based Javascript library for building
+                efective and fast user interfaces.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item lg={5} sm={12}>
+        <Card sx={{ minWidth: 345 }}>
+          <CardActionArea sx={{ display: `${width1200 ? "flex" : "inline"}` }}>
+            <Box
+              sx={{
+                display: "flex",
+                padding: "15px",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+                width: "100%",
+                filter: `drop-shadow(  ${coor[0]}px ${coor[1]}px  5px rgba(0,0,0,0.5))`,
+              }}
+            >
+              <CardMedia
+                sx={{ textShadow: "2px 2px 2px rgba(0,0,0)" }}
+                component="svg"
+                image={sassSvg}
+                alt="sass"
+                width="200px"
+              />
+              {!width600&&<Typography variant="h4" color="text.secondary" fontSize="3rem">
+                SASS
+              </Typography>}
+            </Box>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Styling
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Most popular styling language used with most of libraries.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      {/* row height  equalized */}
+      <Grid item lg={7} sm={12} sx={{ display: "grid", alignItems: "stretch" }}>
+        <Item>
+          <SkillList skillList={skillListStyling}/>
+        </Item>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Skills;
